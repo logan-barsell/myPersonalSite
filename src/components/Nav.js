37d33f000efeedb3c3660e8b908623bddf06b038
Logan Barsell <loganjbars@gmail.com>
2021-09-28 01:00:23 -0700

    semantic-ui--->bootstrap

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,14 +5,22 @@ import './css/nav.css'
 const Nav = () => {
 
     return (
-        <div className="ui container">
+        <div className="navBar container">
+            <button 
+                type="button"
+                class="btn btn-secondary"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </button>
+
             <div 
-                className="navBar ui large tabular fluid four item menu"
-                style={{marginTop: "33px"}}
+                className="nav nav-pills flex-column flex-sm-row"
             >
                 <NavLink 
                     to="/" 
-                    className="item" 
+                    className="flex-sm-fill text-sm-center nav-link" 
                     activeClassName="active" 
                     exact
                 >
@@ -20,52 +28,26 @@ const Nav = () => {
                 </NavLink>
                 <NavLink 
                     to="/Skills"
-                    className="item"
+                    className="flex-sm-fill text-sm-center nav-link"
                 >
                     Skills
                 </NavLink>
                 <NavLink 
                     to="/Portfolio"
-                    className="item"
+                    className="flex-sm-fill text-sm-center nav-link"
                 >
                     Portfolio
                 </NavLink>
                 <NavLink 
                     to="/Contact"
-                    className="item"
+                    className="flex-sm-fill text-sm-center nav-link"
                 >
                     Contact
                 </NavLink>
             </div>
+            
+            
 
-            <div class="ui vertical inverted sidebar menu">
-                <NavLink 
-                    to="/" 
-                    className="item" 
-                    activeClassName="active" 
-                    exact
-                >
-                    Home
-                </NavLink>
-                <NavLink 
-                    to="/Skills"
-                    className="item"
-                >
-                    Skills
-                </NavLink>
-                <NavLink 
-                    to="/Portfolio"
-                    className="item"
-                >
-                    Portfolio
-                </NavLink>
-                <NavLink 
-                    to="/Contact"
-                    className="item"
-                >
-                    Contact
-                </NavLink>
-            </div>
         </div>
     );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,18 +4,17 @@ import '../css/home.css';
 const Home = () => {
     return (
         <div>
-            <div className="ui home container">
-                <div className="ui vertical masthead center aligned segment">
-                    <div classname="ui text container">
-                        <h1 className="ui header">Hello, my name is  
-                            <span className="ui header"> Logan</span>!
+            <div className="home container">
+                <div className="masthead">
+                    <div classname="container">
+                        <h1 className="header">Hello, my name is  
+                            <span className="header"> Logan</span>!
                         </h1>
                         <h2>Front-End Web Developer, at your service.
                         </h2>
                     </div>
                 </div>
             </div>
-            <div className="ui horizontal divider">About Me</div>
         </div>
     );
 }
